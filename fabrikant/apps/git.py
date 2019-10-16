985e353d10a9cc84087056f63aaad7ec7cdc0329
Jens Christian Jensen <jensecj@gmail.com>
2019-10-16 18:22:31 +0200

    split git info fetching into three parts, much simpler

--- fabrikant/apps/git.py
+++ fabrikant/apps/git.py
@@ -2,25 +2,14 @@ from ..util import set_runner
 
 
 @set_runner
-def latest_commit(c, runner, repo):
-    """
-    Return a triplet of the latest commits hash, timestamp, and message.
-    """
-    cmd = "cd {} && git log --format='%H %ai %B' -n 1".format(repo)
-    commit = runner(cmd, hide=True, warn=True).stdout.strip().split()
-    id = commit[0]
-    timestamp = " ".join(commit[1:4])
-    msg = " ".join(commit[4:])
-    return id, timestamp, msg
-
-
-@set_runner
 def latest_commit_hash(c, runner, repo):
     """
     Return the hash of the latest commit.
     """
-    id, _, _ = latest_commit(c, repo, runner=runner)
-    return id
+    cmd = "cd {} && git log --format='%H' -n 1".format(repo)
+    ret = runner(cmd, hide=True, warn=True)
+    if ret.ok:
+        return ret.stdout.strip()
 
 
 @set_runner
@@ -28,8 +17,10 @@ def latest_commit_timestamp(c, runner, repo):
     """
     Return the timestamp of the latest commit.
     """
-    _, timestamp, _ = latest_commit(c, repo, runner=runner)
-    return timestamp
+    cmd = "cd {} && git log --format='%ai' -n 1".format(repo)
+    ret = runner(cmd, hide=True, warn=True)
+    if ret.ok:
+        return ret.stdout.strip()
 
 
 @set_runner
@@ -37,5 +28,8 @@ def latest_commit_message(c, runner, repo):
     """
     Return the message of the latest commit.
     """
-    _, _, msg = latest_commit(c, repo, runner=runner)
-    return msg
+    cmd = "cd {} && git log --format='%B' -n 1".format(repo)
+    ret = runner(cmd, hide=True, warn=True)
+    if ret.ok:
+        return ret.stdout.strip()
+
