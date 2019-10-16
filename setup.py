51ca4a725a3564fb2d23910e7d389727cb01fda2
Jens Christian Jensen <jensecj@gmail.com>
2019-10-16 16:02:17 +0200

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="fabrikant",
+    version="0.0.1",
+    description="",
+    author="Jens Christian Jensen",
+    author_email="jensecj@gmail.com",
+    url="http://github.com/jensecj/fabrikant",
+    install_requires=["fabric>=2.0,<3.0"],
+    packages=find_packages(),
+)
