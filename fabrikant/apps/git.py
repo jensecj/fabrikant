6956524797ae74d5dad7ddf9484ef5642740ff7c
Jens Christian Jensen <jensecj@gmail.com>
2019-10-16 17:36:16 +0200

    added some simple info fetching from git

---
+++ fabrikant/apps/git.py
@@ -0,0 +1,41 @@
+from ..util import set_runner
+
+
+@set_runner
+def latest_commit(c, runner, repo):
+    """
+    Return a triplet of the latest commits hash, timestamp, and message.
+    """
+    cmd = "cd {} && git log --format='%H %ai %B' -n 1".format(repo)
+    commit = runner(cmd, hide=True, warn=True).stdout.strip().split()
+    id = commit[0]
+    date = " ".join(commit[1:4])
+    msg = " ".join(commit[4:])
+    return id, date, msg
+
+
+@set_runner
+def latest_commit_hash(c, runner, repo):
+    """
+    Return the hash of the latest commit.
+    """
+    id, _, _ = latest_commit(c, repo, runner=runner)
+    return id
+
+
+@set_runner
+def latest_commit_timestamp(c, runner, repo):
+    """
+    Return the timestamp of the latest commit.
+    """
+    _, timestamp, _ = latest_commit(c, repo, runner=runner)
+    return timestamp
+
+
+@set_runner
+def latest_commit_message(c, runner, repo):
+    """
+    Return the message of the latest commit.
+    """
+    _, _, msg = latest_commit(c, repo, runner=runner)
+    return msg
