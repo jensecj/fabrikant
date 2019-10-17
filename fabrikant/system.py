e34be6c6ad9c20c8ed239c3f96a578a03f7fa016
Jens Christian Jensen <jensecj@gmail.com>
2019-10-17 23:29:27 +0200

    system.py: added some user and group related functions

--- fabrikant/system.py
+++ fabrikant/system.py
@@ -7,7 +7,7 @@ from .fs import exists
 @set_runner
 def has_program(c, runner, program):
     """
-    Return True if  `program' is in in $PATH.
+    Return True if `program' is in in $PATH.
     """
     cmd = "which {}".format(program)
     return runner(cmd, hide=True, warn=True).ok
@@ -58,7 +58,7 @@ def hostname(c, runner):
 @set_runner
 def current_user(c, runner):
     """
-    Return the current user.
+    Return the name of the current user.
     """
     cmd = "whoami"
     user = runner(cmd, hide=True, warn=True).stdout.strip()
@@ -68,7 +68,7 @@ def current_user(c, runner):
 @set_runner
 def users_online(c, runner):
     """
-    Return all users who are currently logged in.
+    Return a list of names of all users who are currently logged in.
     """
     cmd = "users"
     users = runner(cmd, hide=True, warn=True).stdout.strip().split()
@@ -80,27 +80,54 @@ def user_exists(c, runner, user):
     """
     Return True if `user' exists.
     """
-    cmd = "users"
-    users = runner(cmd, hide=True, warn=True).stdout.strip()
-    return user in users
+    cmd = "id -u {}".format(user)
+    return runner(cmd, hide=True, warn=True).ok
 
 
 @set_runner
-def create_user(c, runner, group):
-    pass
+def create_user(c, runner, user, group=None, shell=None):
+    """
+    Return True if `user' already exists, or creating `user' succeeds.
+    """
+    if user_exists(c, user, runner=runner):
+        return True
+
+    group = "-G {}".format(group) if group else ""
+    shell = "-s {}".format(shell) if shell else ""
+    cmd = "useradd {} {} {}".format(group, shell, user)
+    return runner(cmd, hide=True, warn=True).ok
 
 
-# * groups
+# * accessors
 
 
 @set_runner
 def groups(c, runner):
     """
-    Return all groups on the system.
+    Return a list of all groups on the system.
     """
-    cmd = "groups"
+    cmd = "cat /etc/group | cut -d: -f1"
     groups = runner(cmd, hide=True, warn=True).stdout.strip().split()
     return groups
+
+
+@set_runner
+def user_groups(c, runner, user):
+    """
+    Return a list of all groups `user' is in.
+    Return None if user does not exist.
+    """
+    if not user_exists(c, user, runner=runner):
+        return None
+
+    cmd = "groups {}".format(user)
+    output = runner(cmd, hide=True, warn=True)
+
+    if output.ok:
+        return output.stdout.strip().split()
+
+
+# * predicates
 
 
 @set_runner
@@ -108,11 +135,45 @@ def group_exists(c, runner, group):
     """
     Return True if `group' exists.
     """
-    cmd = "groups"
-    groups = runner(cmd, hide=True, warn=True).stdout.strip()
-    return group in groups
+    return group in groups(c, runner=runner)
 
 
 @set_runner
-def create_group(c, runner, group):
-    pass
+def user_in_group(c, runner, user, group):
+    """
+    Return True if `user' is a member of `group'.
+    Return None if either `user' or `group' does not exist.
+    """
+    if not user_exists(c, user, runner=runner):
+        return None
+
+    if not group_exists(c, group, runner=runner):
+        return None
+
+    groups = user_groups(c, user, runner=runner)
+    return user in groups
+
+
+# * mutators
+
+
+@set_runner
+def user_add_group(c, runner, user, group):
+    """
+    Return True if `user' is already a member of `group', or adding `user' to `group' succeeds.
+    Return None if either `user' or `group' does not exist.
+    """
+    if not user_exists(c, user, runner=runner):
+        return None
+
+    if not group_exists(c, group, runner=runner):
+        return None
+
+    if user_in_group(c, user, group, runner=runner):
+        return True
+
+    group = "-G {}".format(group)
+    cmd = "usermod {} {}".format(group, user)
+    return runner(cmd, hide=True, warn=True).ok
+
+
