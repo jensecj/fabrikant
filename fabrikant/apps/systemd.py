2afc62d25e9b30424b2d358a18901c4bc5699079
Jens Christian Jensen <jensecj@gmail.com>
2019-10-19 20:25:01 +0200

    reordered fs.py to match the other files

--- fabrikant/apps/systemd.py
+++ fabrikant/apps/systemd.py
@@ -1,6 +1,92 @@
 from ..util import set_runner
 
 
+# * accessors
+
+
+@set_runner
+def running_units(c, runner):
+    """
+    Return a list of all running units.
+    """
+    cmd = "systemctl list-units --no-pager --no-legend --state=running | cut -d' ' -f1"
+    units = runner(cmd, hide=True, warn=True).stdout.strip().split()
+    return units
+
+
+@set_runner
+def active_units(c, runner):
+    """
+    Return a list of all active units.
+    """
+    cmd = "systemctl list-units --no-pager --no-legend --state=active | cut -d' ' -f1"
+    units = runner(cmd, hide=True, warn=True).stdout.strip().split()
+    return units
+
+
+@set_runner
+def failed_units(c, runner):
+    """
+    Return a list of all failed units.
+    """
+    cmd = "systemctl list-units --no-pager --no-legend --state=failed | cut -d' ' -f1"
+    units = runner(cmd, hide=True, warn=True).stdout.strip().split()
+    return units
+
+
+@set_runner
+def show(c, runner, service):
+    """
+    Return the output of `systemctl show' for `service'.
+    """
+    cmd = "systemctl show {}".format(service)
+    output = runner(cmd, hide=True, warn=True).stdout.strip()
+    return output
+
+
+@set_runner
+def status(c, runner, service):
+    """
+    Return the output of `systemctl status' for `service'.
+    """
+    cmd = "systemctl status {}".format(service)
+    output = runner(cmd, hide=True, warn=True).stdout.strip()
+    return output
+
+
+# * predicates
+
+
+@set_runner
+def is_active(c, runner, service):
+    """
+    Return True if `service' is active.
+    """
+    cmd = "systemctl is-active {}".format(service)
+    return runner(cmd, hide=True, warn=True).ok
+
+
+@set_runner
+def is_enabled(c, runner, unit):
+    """
+    Return True if `unit' is enabled.
+    """
+    cmd = "systemctl is-enabled {}".format(unit)
+    return runner(cmd, hide=True, warn=True).ok
+
+
+@set_runner
+def is_failed(c, runner, service):
+    """
+    Return True if `service' has failed.
+    """
+    cmd = "systemctl is-failed {}".format(service)
+    return runner(cmd, hide=True, warn=True).ok
+
+
+# * mutators
+
+
 @set_runner
 def enable(c, runner, service):
     """
@@ -53,41 +139,3 @@ def reload(c, runner, service):
     """
     cmd = "systemctl reload {}".format(service)
     return runner(cmd, hide=True, warn=True).ok
-
-
-@set_runner
-def is_active(c, runner, service):
-    """
-    Return True if `service' is active.
-    """
-    cmd = "systemctl is-active {}".format(service)
-    return runner(cmd, hide=True, warn=True).ok
-
-
-@set_runner
-def is_failed(c, runner, service):
-    """
-    Return True if `service' has failed.
-    """
-    cmd = "systemctl is-failed {}".format(service)
-    return runner(cmd, hide=True, warn=True).ok
-
-
-@set_runner
-def show(c, runner, service):
-    """
-    Return the output of `systemctl show' for `service'.
-    """
-    cmd = "systemctl show {}".format(service)
-    output = runner(cmd, hide=True, warn=True).stdout.strip()
-    return output
-
-
-@set_runner
-def status(c, runner, service):
-    """
-    Return the output of `systemctl status' for `service'.
-    """
-    cmd = "systemctl status {}".format(service)
-    output = runner(cmd, hide=True, warn=True).stdout.strip()
-    return output
